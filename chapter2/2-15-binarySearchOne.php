e9016daa3f98bd60108132a7c897ed4fa6deb06f
wanguohui <wanguohui@yahoo.com>
2018-03-13 16:08:26 +0800

    二进制计算相关的其他题目

--- chapter2/2-15-binarySearchOne.php
+++ chapter2/2-15-binarySearchOne.php
@@ -51,6 +51,21 @@ function binarySearch3($n)
 	}
 	return $count;
 }
+//相关题目，用一条语句判断一个整数是否为2的整数次方
+function isInteger($n)
+{
+	return (($n - 1) & $n) == 0;
+}
+//相关题目，判断m要改变多少位才能变成n；
+//思路：先异或再求异或结果中1的位数
+function countM2n($m,$n)
+{
+	if($m < 0 || $n < 0){
+		return false;
+	}
+	return binarySearch3($m ^ $n);
+}
+
 
 //test
 echo binarySearch1(12);
@@ -60,5 +75,12 @@ echo binarySearch2(-12);
 echo '<br />';
 
 echo binarySearch3(-12);
+echo '<br />';
+
+//test 相关题目
+echo isInteger(1024) == true;
+echo '<br />';
+
+echo countM2n(10,13);
 
 ?>
