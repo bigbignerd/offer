a1768328cd1055739ff1f45071ddf1cfe7b54113
bignerd <wanguohui@yahoo.com>
2019-05-21 17:57:50 +0800

    获取数组排列组成的最小数字

---
+++ chapter5/5-45-arrayMinNumber.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * 给定正整数数组，求数组中元素按照某一顺序排列所能得到的最小数字
+ * 如给定["3", "32", "321"]，最小数字应该为:321323
+ * 
+ * @note 数学证明？？？
+ */
+
+//给数组排个序，按照“从小到大”
+function getArrayMinNumber($arr)
+{
+    if (empty($arr)) {
+        return -1;
+    }
+    usort($arr, "compare");
+    foreach($arr as $k => $v) {
+        echo $v;
+    }
+    echo PHP_EOL;
+}
+
+//如果ab拼成的数字 < ba 拼成的数字则称a<b,如332>323 所以32 < 3,32应该排在3的前面
+function compare($a, $b)
+{
+    $n1 = $a.$b;
+    $n2 = $b.$a;
+    return strcmp($n1, $n2);
+}
+
+//test
+$arr = ["3", "32", "321"];
+getArrayMinNumber($arr);
