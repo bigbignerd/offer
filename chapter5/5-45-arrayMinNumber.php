d73615458899ada8f59f011d98fdde6633862523
bignerd <wanguohui@yahoo.com>
2019-05-21 18:32:33 +0800

    add max

--- chapter5/5-45-arrayMinNumber.php
+++ chapter5/5-45-arrayMinNumber.php
@@ -13,10 +13,16 @@ function getArrayMinNumber($arr)
         return -1;
     }
     usort($arr, "compare");
+    echo 'min:';
     foreach($arr as $k => $v) {
         echo $v;
     }
     echo PHP_EOL;
+    echo 'max:';
+    usort($arr, "compare2");
+    foreach($arr as $k => $v) {
+        echo $v;
+    }
 }
 
 //如果ab拼成的数字 < ba 拼成的数字则称a<b,如332>323 所以32 < 3,32应该排在3的前面
@@ -26,6 +32,16 @@ function compare($a, $b)
     $n2 = $b.$a;
     return strcmp($n1, $n2);
 }
+
+//能组成的最大数
+function compare2($a, $b)
+{
+    $n1 = $a.$b;
+    $n2 = $b.$a;
+    return -(strcmp($n1, $n2));
+}
+
+
 
 //test
 $arr = ["3", "32", "321"];
