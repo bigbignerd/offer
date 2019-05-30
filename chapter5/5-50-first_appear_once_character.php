5d964ff061bc6026e607c08595a0bea8481d27fb
bignerd <wanguohui@yahoo.com>
2019-05-30 19:07:52 +0800

    数据流中第一次只出现一次的字符

--- chapter5/5-50-first_appear_once_character.php
+++ chapter5/5-50-first_appear_once_character.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * 给定字符串，求第一个只出现一次的字符
+ * 1、给定字符串，求第一个只出现一次的字符
+ * 2、读取字符流，返回当前第一个只出现一次的字符
  */
 
 function firstAppear($str)
@@ -23,6 +24,46 @@ function firstAppear($str)
     }
 }
 
+//2
+class First
+{
+    private $str = '';
+    private $store = [];
+    private $index = 0;
+    
+    public function insert($c)
+    {
+        $this->str .= $c;
+        if (!isset($this->store[$c])) {
+            $this->store[$c] = $this->index;
+        } else {
+            $this->store[$c] = -1;
+        }
+        $this->index++;
+    }
+
+    public function appear()
+    {
+        foreach($this->store as $k => $v) {
+            if ($v >=0) {
+                echo 'current str is:' . $this->str . PHP_EOL;
+                echo 'current first appear character is:' . $k;
+                echo PHP_EOL;
+                break;
+            }
+        }
+    }
+}
+
 //test
 $str = "abaccdeff";
-firstAppear($str);
+$str = "google";
+//firstAppear($str);
+
+//test2
+$f = new First();
+$len = strlen($str);
+for ($i=0; $i<$len; $i++) {
+    $f->insert($str[$i]);
+    $f->appear();
+}
